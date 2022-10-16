d261e2197cbebef85d2a06c249b834def9b9fada
Royk8 <roy.maestre@udea.edu.co>
2022-10-15 19:11:13 -0500

    primer commit

---
+++ Assets/Scripts/ejemplo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejemplo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("primer commit");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
